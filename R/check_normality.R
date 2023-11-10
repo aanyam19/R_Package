0fcbccc1f9e40829ac4aecf4e6567ae0c6d44921
aanyam19 <aanyam@umich.edu>
2023-11-10 16:38:47 -0500

    check normality

--- R/check_normality.R
+++ R/check_normality.R
@@ -1,14 +1,18 @@
 #' Check Normality
 #' 
 #' @description
-#' returns  
+#' Checks if the true errors of the model satisfy the assumption
+#' of normality. Data points should ideally fall along the 
+#' dotted line. 
 #' 
 #' @param model linear model
 #' 
-#' @returns 
+#' @returns Quantile-Quantile (Q-Q) plot.
 #' 
 #' @examples
-#' 
+#' linear_model <- lm(height ~ weight, data = data)
+#' check_normality(linear_model)
 #' 
 check_normality <- function(model){
+  return(plot(model, which = 2))
 }
