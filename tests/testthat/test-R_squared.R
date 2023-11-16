6b5301c443297ca7d3d2ad9b1f487f53eeabcb2d
aanyam19 <aanyam@umich.edu>
2023-11-16 17:37:04 -0500

    tests

--- tests/testthat/test-R_squared.R
+++ tests/testthat/test-R_squared.R
@@ -2,5 +2,8 @@ test_that("R_squared works", {
   set.seed(123)
   predictor <- sample(seq(1, 100), size = 10, replace = TRUE)
   response <- sample(seq(1, 100), size = 10, replace = TRUE)
-  expect_equal(2 * 2, 4)
+  R2 <- R_squared(predictor, response)
+  model <- lm(response ~ predictor)
+  summary_R2 <- summary(model)$r.squared
+  expect_equal(R2, summary_R2)
 })
